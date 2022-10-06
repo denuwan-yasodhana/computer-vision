8f8be05040258b5b64ead0b1f44862ab0712f66e
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2022-10-06 17:28:40 +0530

    Create Chapter 06.py

---
+++ OpenCV/Chapter 06.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/image.jpg")
+
+
+
+# Joining images Horizontally  (Cannot Resize, Cannot add different channel image)
+imgHor = np.hstack((img, img))
+
+# Joining images Vertically    (Cannot Resize, Cannot add different channel image)
+imgVer = np.vstack((img, img))  
+
+
+# This custom function use for joining different channel images and make horizontal or vertical
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+  
+  
+imgStack = stackImages(0.5, ([img,img,img]))
+cv2.imshow("Using StackImages function", imgStack)
+cv2.waitKey(0)
+  
