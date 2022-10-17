2fe39e257ba80f4b556ad8cfdeddb2017d77ddcf
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2022-10-17 14:50:29 +0530

    Update Chapter 02.py

--- OpenCV/Chapter 02.py
+++ OpenCV/Chapter 02.py
@@ -19,6 +19,41 @@ imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)             # iteratio
 imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
 
 
+# How to change image value using trackbar
+
+import cv2
+import numpy as np
+
+def empty(a):                                                    # This function is used to change trackbar everytime from user, what need to change
+    pass
+  
+  
+ # Create TrackBar
+
+cv2.namedWindow("TrackBars")                                     # Create new window
+cv2.resizeWindow("TrackBars", 640, 240)                          # Use "same name", using for resize
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)        # Create first trackbar  # (what value going to change[Hue = color], which window, min value, max value)
+cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty)
+cv2.createTrackbar("Saturation Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Saturation Max", "TrackBars", 255, 255, empty)
+cv2.createTrackbar("Value Min", "TrackBars", 134, 255, empty)
+cv2.createTrackbar("Value Max", "TrackBars", 255, 255, empty)
+
+
+while True:
+    img = cv2.imread("Resources/image.jpg")
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")            # Get values to variable
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Saturation Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Saturation Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Value Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Value Max", "TrackBars")
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    maskImage = cv2.inRange(imgHSV, lower, upper)                 # Set values to image
 
 
 
